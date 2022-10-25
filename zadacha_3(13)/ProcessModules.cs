d0da57fc952ec257dffcc2c80ae6c5f292fa122c
Dreaws Walles <85035654+DreawsWalles@users.noreply.github.com>
2022-10-25 19:56:37 +0300

    Произвести Snapshot процессов

---
+++ zadacha_3(13)/ProcessModules.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_3_13_
+{
+    public class ProcessModules
+    {
+        public ProcessEntry32 Process { get; set; }
+
+        public uint sizeModulse { get; set; }
+
+        public ProcessModules()
+        {
+            sizeModulse = 0;
+        }
+    }
+}
